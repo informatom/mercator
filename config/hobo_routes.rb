fb50cf7ed352f43213b91d4ffa4ac0dde8a88433
Stefan Haslinger <stefan.haslinger@mittenin.at>
2013-10-15 09:14:34 +0200

    initial hobo app

---
+++ config/hobo_routes.rb
@@ -0,0 +1,26 @@
+# This is an auto-generated file: don't edit!
+# You can add your own routes in the config/routes.rb file
+# which will override the routes in this file.
+
+Mercator::Application.routes.draw do
+
+
+  # Resource routes for controller users
+  resources :users, :only => [:edit, :show, :create, :update, :destroy] do
+    collection do
+      post 'signup', :action => 'do_signup'
+      get 'signup'
+    end
+    member do
+      get 'account'
+      put 'reset_password', :action => 'do_reset_password'
+      get 'reset_password'
+    end
+  end
+
+  # User routes for controller users
+  match 'login(.:format)' => 'users#login', :as => 'user_login'
+  get 'logout(.:format)' => 'users#logout', :as => 'user_logout'
+  match 'forgot_password(.:format)' => 'users#forgot_password', :as => 'user_forgot_password'
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mercator::Application.config.secret_token = 'e4e24a41040f0e1ea108a0d4a25c21d680bfa27b9411613a888fc92bea258f179f62c49ed02ab2a6a7f4589cac01841bb9e161adaa3c19d61607968710f1f763'
