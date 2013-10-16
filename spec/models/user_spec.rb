db6d508d20e9270b893fdbc253590415ad673792
Stefan Haslinger <stefan.haslinger@mittenin.at>
2013-10-16 16:42:54 +0200

    erste tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe User do
-  it "is valid with a name, email_address and administrator"
-  it "is invalid without a name"
+  it "is valid with a name, email_address and administrator" do
+    user = User.new( name: "Stefan", email_address: "stefan.haslinger@mittenin.at", administrator: true )
+    expect(user).to be_valid
+  end
+
+  it "is invalid without a name" do
+    expect(User.new(name: nil)).to have(1).errors_on(:name)
+  end
 end
