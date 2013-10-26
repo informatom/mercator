d440b42fc7d644d102aff22226feabab69ba4f92
Stefan Haslinger <stefan.haslinger@mittenin.at>
2013-10-26 18:11:18 +0200

    ckeditor with uploads

---
+++ app/controllers/ckeditor/attachment_files_controller.rb
@@ -0,0 +1,28 @@
+class Ckeditor::AttachmentFilesController < Ckeditor::ApplicationController
+
+  def index
+    @attachments = Ckeditor.attachment_file_model.find_all(ckeditor_attachment_files_scope)
+    respond_with(@attachments, :layout => "ckeditor/application")
+  end
+
+  def create
+    @attachment = Ckeditor::AttachmentFile.new
+	  respond_with_asset(@attachment)
+  end
+
+  def destroy
+    @attachment.destroy
+    respond_with(@attachment, :location => attachment_files_path)
+  end
+
+  protected
+
+    def find_asset
+      @attachment = Ckeditor.attachment_file_model.get!(params[:id])
+    end
+
+    def authorize_resource
+      model = (@attachment || Ckeditor::AttachmentFile)
+      @authorization_adapter.try(:authorize, params[:action], model)
+    end
+end
--- config/hobo_routes.rb
+++ config/hobo_routes.rb
@@ -5,21 +5,8 @@
 Mercator::Application.routes.draw do
 
 
-  # Resource routes for controller products
-  resources :products do
-    member do
-      put 'activate', :action => 'do_activate'
-      get 'activate'
-      put 'announce', :action => 'do_announce'
-      get 'announce'
-      put 'release', :action => 'do_release'
-      get 'release'
-      put 'deactivate', :action => 'do_deactivate'
-      get 'deactivate'
-      put 'reactivate', :action => 'do_reactivate'
-      get 'reactivate'
-    end
-  end
+  # Resource routes for controller addresses
+  resources :addresses
 
 
   # Resource routes for controller categories
@@ -39,8 +26,21 @@ Mercator::Application.routes.draw do
   end
 
 
-  # Resource routes for controller content_elements
-  resources :content_elements
+  # Resource routes for controller products
+  resources :products do
+    member do
+      put 'activate', :action => 'do_activate'
+      get 'activate'
+      put 'announce', :action => 'do_announce'
+      get 'announce'
+      put 'release', :action => 'do_release'
+      get 'release'
+      put 'deactivate', :action => 'do_deactivate'
+      get 'deactivate'
+      put 'reactivate', :action => 'do_reactivate'
+      get 'reactivate'
+    end
+  end
 
 
   # Resource routes for controller users
@@ -62,7 +62,11 @@ Mercator::Application.routes.draw do
   match 'forgot_password(.:format)' => 'users#forgot_password', :as => 'user_forgot_password'
 
 
-  # Resource routes for controller addresses
-  resources :addresses
+  # Resource routes for controller content_elements
+  resources :content_elements
+
+  namespace :ckeditor do
+
+  end
 
 end
