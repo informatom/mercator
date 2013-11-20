1bb655e9f3c5b504017a73dd9f5c217f1f12f77c
Stefan Haslinger <stefan.haslinger@mittenin.at>
2013-11-20 14:33:17 +0100

    no changes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,9 +3,4 @@ class OrdersController < ApplicationController
   hobo_model_controller
 
   auto_actions :all
-
-  def index
-    hobo_index Order.apply_scopes(:search => [params[:search], :billing_method, :shipping_method],
-        :order_by => parse_sort_param(:created_at))
-  end
 end
