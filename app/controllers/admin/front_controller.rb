86e5077271e36cea5e1994ab8ea4bed3d8b86e6e
Stefan Haslinger <stefan.haslinger@mittenin.at>
2013-11-22 14:04:47 +0100

    admin controllers

--- app/controllers/admin/front_controller.rb
+++ app/controllers/admin/front_controller.rb
@@ -1,4 +1,4 @@
-class Admin::FrontController < ApplicationController
+class Admin::FrontController < Admin::AdminSiteController
 
   hobo_controller
 
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,4 @@
-class Admin::UsersController < ApplicationController
+class Admin::UsersController < Admin::AdminSiteController
 
   hobo_model_controller
 
