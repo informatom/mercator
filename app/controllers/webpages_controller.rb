321c9e559a1cdac2bfcd7b74645c9baf2d9e897f
Stefan Haslinger <stefan.haslinger@mittenin.at>
2014-01-13 15:25:44 +0100

    page show

--- app/controllers/webpages_controller.rb
+++ app/controllers/webpages_controller.rb
@@ -2,4 +2,10 @@ class WebpagesController < ApplicationController
 
   hobo_model_controller
   auto_actions :show
+
+  def show
+    hobo_show do
+      render "page_templates/" + @this.page_template.name
+    end
+  end
 end
