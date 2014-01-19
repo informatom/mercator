9bf3daab69f7c13622e6dc60720712311f74beea
Stefan Haslinger <stefan.haslinger@mittenin.at>
2014-01-19 20:55:26 +0100

    product comparison

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -30,8 +30,36 @@ class ProductsController < ApplicationController
     end
   end
 
+  Propertyline = Struct.new(:position, :group, :name, :values, :isgroup)
+
   def comparison
     self.this = Product.where(id: session[:compared]).paginate(:page => 1, :per_page => Product.count)
+
+    @property_lines = Array.new()
+    this.each_with_index do |product, i|
+      product.property_groups.each do |pg|
+        if @line = @property_lines.find { |e| e[:name] == pg.name }
+          @group = @line.group
+        else
+          @property_lines << Propertyline.new(pg.position, pg.id, pg.name, Array.new(this.count), true)
+          @group = pg.id
+        end
+
+        pg.properties.each do |p|
+          @value = p.description ? p.description : p.value + " " + p.unit
+          if @line = @property_lines.find { |e| e[:name] == p.name &&
+                                                e[:group] == @group &&
+                                                e[:isgroup] == false }
+            @line.values[i] = @value
+          else
+            @line = Propertyline.new(p.position, pg.id, p.name, Array.new(this.count), false)
+            @line.values[i] = @value
+            @property_lines << @line
+          end
+        end
+      end
+    end
+
     hobo_index
   end
 
