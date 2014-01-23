271ed120d9f045dd379f9d2b0cccfefb5a0c9dab
Stefan Haslinger <stefan.haslinger@mittenin.at>
2014-01-23 09:28:17 +0100

    formatting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,8 @@ class UsersController < ApplicationController
     hobo_create do
       if valid?
         self.current_user = this
-        flash[:notice] = t("hobo.messages.you_are_site_admin", :default=>"You are now the site administrator")
+        flash[:notice] = t("hobo.messages.you_are_site_admin",
+                         :default=>"You are now the site administrator")
         redirect_to home_page
       end
     end
