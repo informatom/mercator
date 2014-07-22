6ce1bfb0615644fdee5b0eb190f528d4baa9d9d4
Stefan Haslinger <stefan.haslinger@mittenin.at>
2014-07-22 12:03:32 +0200

    fix friendly_id in admin

--- app/controllers/admin/webpages_controller.rb
+++ app/controllers/admin/webpages_controller.rb
@@ -17,6 +17,11 @@ class Admin::WebpagesController < Admin::AdminSiteController
     end
   end
 
+  def show
+    @this = Webpage.friendly.find(params[:id])
+    hobo_show
+  end
+
 protected
   def parse_pages(pages_array, parent)
     pages_array.each_with_index do |page_hash, position|
