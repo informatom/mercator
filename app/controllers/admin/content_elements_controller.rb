af516dd4d48592272aef3412ef98e1d10568fae7
Stefan Haslinger <stefan.haslinger@mittenin.at>
2014-08-13 15:08:02 +0200

    content_element admin search

--- app/controllers/admin/content_elements_controller.rb
+++ app/controllers/admin/content_elements_controller.rb
@@ -2,4 +2,18 @@ class Admin::ContentElementsController < Admin::AdminSiteController
 
   hobo_model_controller
   auto_actions :all
+
+  def index
+    if params[:search].present?
+      @search = params[:search].split(" ").map{|word| "%" + word + "%"}
+      self.this = ContentElement.paginate(:page => params[:page])
+                                .where{(name_de.matches_any my{@search}) |
+                                       (name_en.matches_any my{@search})}
+                                .order_by(parse_sort_param(:name_de, :name_en, :this))
+    else
+      self.this = ContentElement.paginate(:page => params[:page])
+                                .order_by(parse_sort_param(:name_de, :name_en, :this))
+    end
+    hobo_index
+  end
 end
