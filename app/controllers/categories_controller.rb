a2ffc4540b58c467dd4b9a5228d87cb052b6bdf1
Stefan Haslinger <stefan.haslinger@mittenin.at>
2014-09-02 14:11:44 +0200

    search filter working

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -24,18 +24,49 @@ class CategoriesController < ApplicationController
                                },
                                facets: this.filters.values.flatten)
 
-      @products = Product.search(query: {
-                                   bool: {
-                                     must: [ { match: { category_ids: params[:id].to_i } },
-                                             { match: { state: 'active' } }
-                                           ] + matcharray
-                                   }
-                                 }).results
+      if params[:pricelow]
+        @products = Product.search(query: {
+                                     bool: {
+                                       must: [ { match: { category_ids: params[:id].to_i } },
+                                               { match: { state: 'active' } },
+                                               { range: {
+                                                  price: {
+                                                    gte: params[:pricelow],
+                                                    lte: params[:pricehigh]
+                                                    }
+                                                  }
+                                               }
+                                             ] + matcharray
+                                     }
+                                   }).results
 
-      if @products.any?
-        prices = @products.*.determine_price
-        @min = prices.min.round
-        @max = (prices.max + 0.5).round
+        @category_products = Product.search(query: {
+                                              bool: {
+                                                must: [ { match: { category_ids: params[:id].to_i } },
+                                                        { match: { state: 'active' } } ]
+                                              }
+                                            }).results
+      else
+        @products = Product.search(query: {
+                                     bool: {
+                                       must: [ { match: { category_ids: params[:id].to_i } },
+                                               { match: { state: 'active' } }
+                                             ] + matcharray
+                                     }
+                                   }).results
+        @category_products = @products
+      end
+
+      prices = @category_products.*.determine_price
+      @min = prices.min.round
+      @max = (prices.max + 0.5).round
+
+      if params[:pricelow]
+        @minslider = params[:pricelow]
+        @maxslider = params[:pricehigh]
+      else
+        @minslider = @min
+        @maxslider = @max
       end
     end
   end
