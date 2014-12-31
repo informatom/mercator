151339ebd33fb862c42b76efbd5b6e68d0b4b8c3
Stefan Haslinger <stefan.haslinger@mittenin.at>
2014-12-31 10:29:42 +0100

    proper assets in production 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,9 +19,7 @@ Mercator::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  Rails.application.config.assets.precompile += ['front.css, front.js']
 
   ActionMailer::Base.smtp_settings = {
     :domain => CONFIG[:smtp_domain],
