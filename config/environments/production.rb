971948dbb08cc6fecd696b1d4ea1274f70041083
Stefan Haslinger <stefan.haslinger@mittenin.at>
2014-12-31 19:47:28 +0100

    asset precompilation 12

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ Mercator::Application.configure do
     'contentmanager.js', 'contracting.css', 'contracting.js', 'front.css', 'front.js',
     'productmanager.css', 'productmanager.js', 'sales.css', 'sales.js',
     'contentmanager/index/index.js',
+    'productmanager/front/index.js', 'productmanager/price_manager/index.js',
+    'productmanager/property_manager/index.js', 'productmanager/relation_manager/index.js',
     'i18n.js', 'i18n/cm.js', 'i18n/pm.js'  ]
 
   ActionMailer::Base.smtp_settings = {
