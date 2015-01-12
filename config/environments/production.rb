c2fc3b25286e5fc4262eabf580d23626752dd071
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-01-12 16:34:12 +0100

    podlove assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Mercator::Application.configure do
     'productmanager/front/index.js', 'productmanager/price_manager/index.js',
     'productmanager/property_manager/index.js', 'productmanager/relation_manager/index.js',
     'i18n.js', 'i18n/cm.js', 'i18n/pm.js',
-    'jquery', 'jquery-migrate', 'podlove-web-player-rails/index.js' ]
+    'jquery.js', 'jquery-migrate.js', 'podlove-web-player-rails/index.js']
 
   ActionMailer::Base.smtp_settings = {
     :domain => CONFIG[:smtp_domain],
