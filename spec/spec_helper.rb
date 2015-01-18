4643c58a27790cc5269b57a7ac9d0f5304c44205
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-01-18 10:10:32 +0100

    fixture configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,13 +9,8 @@ include ActionDispatch::TestProcess
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  # config.mock_with :mocha
-
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
   # HAS 20131113: For Database Cleaner
-  #config.use_transactional_fixtures = true
+  # config.use_transactional_fixtures = true
   config.use_transactional_fixtures = false
   config.infer_base_class_for_anonymous_controllers = false
   config.order = "random"
