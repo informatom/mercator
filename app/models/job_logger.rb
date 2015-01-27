33f61d9e90da2cd813ab00fbbc05315ee7c167e4
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-01-27 10:31:15 +0100

    log jobs only to database

--- app/models/job_logger.rb
+++ app/models/job_logger.rb
@@ -1,32 +1,22 @@
 class JobLogger
 
   def self.info(message)
-    @joblogger ||= Logger.new("#{Rails.root}/log/#{Rails.env}_jobs.log")
-    @joblogger.ap(message, :info) unless message.nil?
     Logentry.create(severity: :info, message: message)
   end
 
   def self.warn(message)
-    @joblogger ||= Logger.new("#{Rails.root}/log/#{Rails.env}_jobs.log")
-    @joblogger.ap(message, :warn) unless message.nil?
     Logentry.create(severity: :warn, message: message)
   end
 
   def self.error(message)
-    @joblogger ||= Logger.new("#{Rails.root}/log/#{Rails.env}_jobs.log")
-    @joblogger.ap(message, :error) unless message.nil?
     Logentry.create(severity: :error, message: message)
   end
 
   def self.fatal(message)
-    @joblogger ||= Logger.new("#{Rails.root}/log/#{Rails.env}_jobs.log")
-    @joblogger.ap(message, :fatal) unless message.nil?
     Logentry.create(severity: :fatal, message: message)
   end
 
   def self.debug(message)
-    @joblogger ||= Logger.new("#{Rails.root}/log/#{Rails.env}_jobs.log")
-    @joblogger.ap(message, :debug) unless message.nil?
     Logentry.create(severity: :debug, message: message)
   end
 end
