28aece5cf78cd09eef1c7cfc29e190b9a5597f32
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-04-01 14:26:58 +0200

    25 suggestions in product suggestion typeahead

--- app/controllers/sales/products_controller.rb
+++ app/controllers/sales/products_controller.rb
@@ -1,5 +1,5 @@
 class Sales::ProductsController < Sales::SalesSiteController
 
   hobo_model_controller
-  autocomplete :number, :query_scope => [:active_and_number_contains]
+  autocomplete :number, {:query_scope => :active_and_number_contains, limit: 25}
 end
