793c7c119ae8a15986d442bc135acdc9a8f528fa
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-04-03 19:10:27 +0200

    product comparison, inventory dervation

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -75,7 +75,7 @@ class ConversationsController < ApplicationController
   def suggestions
     response.headers.delete('X-Frame-Options')
     @conversation = Conversation.find(params[:id])
-    @this = @products = @conversation.products.paginate(:page => 1, :per_page => @conversation.products.count)
+    self.this = @products = @conversation.products.paginate(:page => 1, :per_page => @conversation.products.count)
     hobo_index
   end
 end
