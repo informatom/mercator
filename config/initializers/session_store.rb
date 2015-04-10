be047e13125523db61669d4962969670b5cfd93a
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-04-10 14:43:06 +0200

    cookie session for 30 days

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Mercator::Application.config.session_store :cookie_store, key: '_mercator_session', domain: :all
+Mercator::Application.config.session_store :cookie_store, key: '_mercator_session', domain: :all, :expire_after => 2592000
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
