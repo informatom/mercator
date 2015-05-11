f80918e45f748592e39260bc598c3cf2d4897ccd
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-05-11 11:23:53 +0200

    model spec

--- spec/factories/constants.rb
+++ spec/factories/constants.rb
@@ -4,4 +4,9 @@ FactoryGirl.define do
     key "Example key"
     value "Example value"
   end
+
+  factory :office_hours do
+    key :office_hours
+    value '{MON: ["8:30", "17:00"], TUE: ["8:30", "17:00"], WED: ["8:30", "17:00"], THU: ["8:30", "17:00"], FRI: ["8:30", "12:30"]}'
+  end
 end
--- spec/models/constant_spec.rb
+++ spec/models/constant_spec.rb
@@ -11,4 +11,25 @@ describe Constant do
   it "is versioned" do
     is_expected.to respond_to :versions
   end
+
+
+# Class Methods
+
+  context "office hours" do
+    it "returns false out of office hours" do
+      monday_night = Time.new(2015, 05, 11, 4, 00, 0, "+00:00")
+      expect(Constant.office_hours?(time: monday_night)).eql? false
+    end
+
+    it "returns true during office hours" do
+      monday_afternoon = Time.new(2015, 05, 11, 14, 00, 0, "+00:00")
+      expect(Constant.office_hours?(time: monday_afternoon)).eql? true
+    end
+  end
+
+  context "pretty_office_hours" do
+    it "returns a string" do
+      expect(Constant.pretty_office_hours.class).eql? String
+    end
+  end
 end
