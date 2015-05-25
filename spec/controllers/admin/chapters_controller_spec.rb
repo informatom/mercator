187e54d24d7c66bb481531fef43d3b94031fccae
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-05-25 10:46:43 +0200

    admin chapters controller tests

--- spec/controllers/admin/chapters_controller_spec.rb
+++ spec/controllers/admin/chapters_controller_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
-describe Admin::ChaptersController do
+describe Admin::ChaptersController, :type => :controller do
+
+  describe "crud actions" do
+    before :each do
+      no_redirects and act_as_admin
+
+      @instance = create(:chapter)
+      @invalid_attributes = attributes_for(:chapter, start: nil)
+    end
+
+    it_behaves_like("crud actions")
+  end
 end
