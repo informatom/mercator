7a0595963d45c0d96ce93ef593712e603d101289
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-05-25 10:43:59 +0200

    admin categorizations controller tests

--- spec/controllers/admin/blogposts_controller_spec.rb
+++ spec/controllers/admin/blogposts_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
-describe Admin::BlogpostsController do
+describe Admin::BlogpostsController, :type => :controller do
 
+  describe "crud actions" do
+    before :each do
+      no_redirects and act_as_admin
+
+      @instance = create(:blogpost)
+      @invalid_attributes = attributes_for(:blogpost, title_de: nil)
+    end
+
+    it_behaves_like("crud actions")
+  end
 end
--- spec/controllers/admin/categorizations_controller_spec.rb
+++ spec/controllers/admin/categorizations_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
-describe Admin::CategorizationsController do
+describe Admin::CategorizationsController, :type => :controller do
 
+  describe "crud actions" do
+    before :each do
+      no_redirects and act_as_admin
+
+      @instance = create(:categorization)
+      @invalid_attributes = attributes_for(:categorization, position: nil)
+    end
+
+    it_behaves_like("crud actions")
+  end
 end
