b180bffe7004e4af2ea1915bb12e331c0a4e17a5
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-05-26 09:38:15 +0200

    admin offers controller tests

---
+++ spec/controllers/admin/offers_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe Admin::OffersController, :type => :controller do
+
+  describe "crud actions" do
+    before :each do
+      no_redirects and act_as_admin
+
+      @instance = create(:offer)
+      @invalid_attributes = attributes_for(:offer, valid_until: nil)
+    end
+
+    it_behaves_like("crud except destroy")
+
+    describe 'DELETE #destroy' do
+      it "redirects to #index" do
+        delete :destroy, id: @instance
+        expect(response).to redirect_to admin_offers_path
+      end
+    end
+  end
+end
