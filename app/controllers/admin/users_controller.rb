8288edcee1f6cff5217416b1f0d0b2b97529aab2
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-05-27 15:40:06 +0200

    admin users controller tests

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,10 +6,10 @@ class Admin::UsersController < Admin::AdminSiteController
   autocomplete :email_address
 
   def index
-    self.this = User.paginate(:page => params[:page])
-                    .search([params[:search], :surname, :first_name, :email_address])
-                    .order_by(parse_sort_param(:surname, :email_address, :last_login_at, :firstname,
-                                               :sales, :sales_manager, :administrator, :login_count, :logged_in))
+    self.this = @users = User.paginate(:page => params[:page])
+                             .search([params[:search], :surname, :first_name, :email_address])
+                             .order_by(parse_sort_param(:surname, :email_address, :last_login_at, :firstname,
+                                                        :sales, :sales_manager, :administrator, :login_count, :logged_in))
     hobo_index
   end
 end
--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -1,5 +1,38 @@
 require 'spec_helper'
 
-describe Admin::UsersController do
+describe Admin::UsersController, :type => :controller do
 
+  describe "crud actions" do
+    before :each do
+      no_redirects and act_as_admin
+
+      @instance = create(:user)
+      @invalid_attributes = attributes_for(:user, email_address: nil)
+    end
+
+    it_behaves_like("crud actions")
+
+
+    describe "GET #index" do
+      it "searches for firstname" do
+        get :index, search: "John"
+        expect(assigns(:users)).to match_array([@instance])
+      end
+
+      it "searches for surname" do
+        get :index, search: "Doe"
+        expect(assigns(:users)).to match_array([@instance])
+      end
+
+      it "searches for email address" do
+        get :index, search: "john.doe@informatom.com"
+        expect(assigns(:users)).to match_array([@instance])
+      end
+
+      it "searches returns nil if nothing found" do
+        get :index, search: "not in user record"
+        expect(assigns(:users)).to match_array([])
+      end
+    end
+  end
 end
