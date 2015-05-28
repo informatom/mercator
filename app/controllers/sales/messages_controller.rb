0a920aea78f8e5160f51b66c6d970262daf247d7
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-05-28 09:27:06 +0200

    sales messages controller tests

--- app/controllers/sales/messages_controller.rb
+++ app/controllers/sales/messages_controller.rb
@@ -7,12 +7,13 @@ class Sales::MessagesController < Sales::SalesSiteController
   def create
     hobo_create do
       this.sender = current_user
-      this.save
-      PrivatePub.publish_to("/" + CONFIG[:system_id] + "/conversations/"+ this.conversation.id.to_s,
-                            type: "messages")
-      PrivatePub.publish_to("/" + CONFIG[:system_id] + "/personal/"+ this.reciever_id.to_s,
-                            sender: this.sender.name,
-                            content: this.content)
+      if this.save
+        PrivatePub.publish_to("/" + CONFIG[:system_id] + "/conversations/"+ this.conversation.id.to_s,
+                              type: "messages")
+        PrivatePub.publish_to("/" + CONFIG[:system_id] + "/personal/"+ this.reciever_id.to_s,
+                              sender: this.sender.name,
+                              content: this.content)
+      end
     end
   end
 end
--- spec/controllers/sales/messages_controller_spec.rb
+++ spec/controllers/sales/messages_controller_spec.rb
@@ -1,5 +1,42 @@
 require 'spec_helper'
 
-describe Sales::MessagesController do
+describe Sales::MessagesController, :type => :controller do
 
+  describe "crud actions" do
+    before :each do
+      no_redirects and act_as_sales
+
+      @conversation = create(:conversation)
+      @instance = create(:message, conversation: @conversation)
+      @invalid_attributes = attributes_for(:message, sender_id: @conversation.customer.id,
+                                                     conversation_id: @conversation.id,
+                                                     reciever_id: @conversation.consultant.id,
+                                                     content: nil)
+    end
+
+    it_behaves_like("crud except destroy")
+
+
+    describe 'DELETE #destroy' do
+      it "redirects to #index" do
+        act_as_admin
+        delete :destroy, id: @instance
+        expect(response).to be_redirect
+      end
+    end
+
+
+    describe 'POST #create' do
+      it "publishes message" do
+        expect(PrivatePub).to receive(:publish_to).with("/0004/conversations/"+ @instance.conversation.id.to_s,
+                                                        type: "messages")
+        expect(PrivatePub).to receive(:publish_to).with("/0004/personal/"+ @instance.reciever.id.to_s,
+                                                        sender: " Sammy Sales Representative",
+                                                        content: @instance.content)
+
+        post :create, message: attributes_for(:message, conversation_id: @conversation.id,
+                                                        sender_id: @conversation.customer.id, reciever_id: @conversation.consultant.id)
+      end
+    end
+  end
 end
