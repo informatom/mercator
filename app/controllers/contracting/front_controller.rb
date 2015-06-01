070714cc13f812df406a736054daf07b78b3be74
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-06-01 09:56:38 +0200

    contracting front controller specs

--- app/controllers/contracting/front_controller.rb
+++ app/controllers/contracting/front_controller.rb
@@ -3,11 +3,4 @@ class Contracting::FrontController < Contracting::ContractingSiteController
   hobo_controller
 
   def index; end
-
-
-  def search
-    if params[:query]
-      site_search(params[:query])
-    end
-  end
 end
--- spec/controllers/contracting/front_controller_spec.rb
+++ spec/controllers/contracting/front_controller_spec.rb
@@ -2,4 +2,20 @@ require 'spec_helper'
 
 describe Contracting::FrontController, :type => :controller do
 
+  describe "before_filters" do
+    before :each do
+      act_as_user
+    end
+
+    it "should check for sales_required" do
+      no_redirects
+      expect(controller).to receive(:sales_required)
+      get :index
+    end
+
+    it "redirects to user login path unless user is sales" do
+      get :index
+      expect(response).to redirect_to root_path(locale: nil).chomp("/")
+    end
+  end
 end
