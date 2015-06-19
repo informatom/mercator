c6c66963d16bf9ec340c87f6ca1cd818814e6a12
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-06-20 00:22:32 +0200

    mpay24 admin payment controller specs

--- spec/mercator_mpay24/controllers/admin/payments_controller_spec.rb
+++ spec/mercator_mpay24/controllers/admin/payments_controller_spec.rb
@@ -26,10 +26,3 @@ describe MercatorMpay24::Admin::PaymentsController, :type => :controller do
     end
   end
 end
-
-# Started GET "/mercator_mpay24/admin/payments/68/check_confirmation?locale=de" for 127.0.0.1 at 2015-06-19 20:08:24 +0200
-# Processing by MercatorMpay24::Admin::PaymentsController#check_confirmation as HTML
-#  Parameters: {"locale"=>"de", "id"=>"68"}
-
-
-# No route matches {:action=>"check_confirmation", :controller=>"mercator_mpay24/admin/payments", :id=>"1"}
