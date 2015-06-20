5ee39390cf9265cabaf502bcc485e1d52aad3c10
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-06-20 08:52:31 +0200

    moved confirmations controller out of mpay namespace

--- spec/mercator_mpay24/controllers/admin/payments_controller_spec.rb
+++ spec/mercator_mpay24/controllers/admin/payments_controller_spec.rb
@@ -1,26 +1,25 @@
 require 'spec_helper'
 
-describe MercatorMpay24::Admin::PaymentsController, :type => :controller do
-  routes { MercatorMpay24::Engine.routes }
-
+describe Admin::PaymentsController, :type => :controller do
   describe "GET #check_confirmation" do
     before :each do
+      no_redirects and act_as_admin
       @payment = create(:payment)
     end
 
     it "finds the payment" do
-      allow(@payment).to receive(:check_transaction_status).and_return(:true)
+      allow_any_instance_of(MercatorMpay24::Payment).to receive(:check_transaction_status).and_return(:true)
       get :check_confirmation, id: @payment.id
       expect(assigns(:payment)).to eql @payment
     end
 
     it "check transaction status" do
-      expect(@payment).to receive(:check_transaction_status).and_return(:true)
+      allow_any_instance_of(MercatorMpay24::Payment).to receive(:check_transaction_status).and_return(:true)
       get :check_confirmation, id: @payment.id
     end
 
     it "redirects to admin order path" do
-      allow(@payment).to receive(:check_transaction_status).and_return(:true)
+      allow_any_instance_of(MercatorMpay24::Payment).to receive(:check_transaction_status).and_return(:true)
       get :check_confirmation, id: @payment.id
       expect(response.body).to redirect_to("/admin/orders/" + @payment.order.id.to_s)
     end
