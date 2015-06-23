41747ff2aeb751b10a1c926d03432574bbcf665e
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-06-23 13:04:05 +0200

    mesonic price model specs

--- spec/mercator_mesonic/models/price.rb
+++ spec/mercator_mesonic/models/price.rb
@@ -6,11 +6,11 @@ describe MercatorMesonic::Price do
 
   context "setup" do
     it "knows its table name" do
-      expect(MercatorMesonic::Price.table_name).to eql "MESOKEY"
+      expect(MercatorMesonic::Price.table_name).to eql "T043"
     end
 
     it "knows its primary key" do
-      expect(MercatorMesonic::Price.primary_key).to eql "mesoprim"
+      expect(MercatorMesonic::Price.primary_key).to eql "MESOKEY"
     end
   end
 
@@ -21,25 +21,44 @@ describe MercatorMesonic::Price do
     end
 
     it "has a scope mesocomp" do
-      expect(MercatorMesonic::Price.unscoped.mesocomp.pluck(:mesoprim))
-                            .to match_array MercatorMesonic::Price.unscoped.where(mesocomp: @mesocomp).pluck(:mesoprim)
+      expect(MercatorMesonic::Price.unscoped.mesocomp.limit(100).pluck(:MESOKEY))
+                            .to match_array MercatorMesonic::Price.unscoped.where(mesocomp: @mesocomp).limit(100).pluck(:MESOKEY)
     end
 
     it "has a scope mesoyear" do
-      expect(MercatorMesonic::Price.unscoped.mesoyear.pluck(:mesoprim))
-                            .to match_array MercatorMesonic::Price.unscoped.where(mesoyear: @mesoyear).pluck(:mesoprim)
+      expect(MercatorMesonic::Price.unscoped.mesoyear.pluck(:MESOKEY))
+                            .to match_array MercatorMesonic::Price.unscoped.where(mesoyear: @mesoyear).pluck(:MESOKEY)
     end
 
 
     it "performs the default scope" do
-      expect(MercatorMesonic::Price.pluck(:mesoprim))
-                            .to match_array MercatorMesonic::Price.unscoped.mesocomp.mesoyear..where(c002: 3).pluck(:mesoprim)
+      expect(MercatorMesonic::Price.pluck(:MESOKEY))
+                            .to match_array MercatorMesonic::Price.unscoped.mesocomp.mesoyear.where(c002: 3).pluck(:MESOKEY)
     end
 
     it "has a scope by_kontonummer" do
-      expect(MercatorMesonic::Price.by_customer(FIXME!!!).*.c112.uniq).to match_array [FIXME!!!]
+      expect(MercatorMesonic::Price.by_customer("661016").*.c003.uniq).to match_array ["661016"]
     end
 
+    it "has a scope by date" do
+      expect(MercatorMesonic::Price.for_date(Date.today).count < MercatorMesonic::Price.all.count).to eql true
+    end
+
+    it "has a scope by_group_through_customer" do
+      expect(MercatorMesonic::Price.by_group_through_customer("011903").count > 0).to eql true
+    end
+
+    it "has a scope group" do
+      expect(MercatorMesonic::Price.group("011903").count > 0).to eql true
+    end
+
+    it "has a scope by_group" do
+      expect(MercatorMesonic::Price.by_group("160").count > 0).to eql true
+    end
+
+    it "has a scope regular" do
+      expect(MercatorMesonic::Price.regular.count > 0).to eql true
+    end
   end
 
 
@@ -51,7 +70,12 @@ describe MercatorMesonic::Price do
 
 
   it "returns c013 fo to_s" do
-    @price = MercatorMesonic::Price.find_by(mesoprim: "000880-2004-1380")
-    expect(@price.to_s).to eql "000880"
+    @price = MercatorMesonic::Price.find_by(MESOKEY: 1395561)
+    expect(@price.to_s).to eql 150.0
+  end
+
+  it "returns c013 when called price" do
+    @price = MercatorMesonic::Price.find_by(MESOKEY: 1395561)
+    expect(@price.price).to eql 150.0
   end
 end
