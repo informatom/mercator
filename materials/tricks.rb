d681d62bf5d4ba058212c39bb7c349ee1095f3ba
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-09-25 11:26:10 +0200

    some more tricks

--- materials/tricks.rb
+++ materials/tricks.rb
@@ -2,7 +2,8 @@
 Recommendation.where.not(product_id: Product.pluck("id"))
 
 # find Instances without related Instances
-Product.includes(:values).where( :values => { :product_id => nil } )
+Product.includes(:values).where( values: { product_id: nil } )
+Inventory.includes(:prices).where(prices: {inventory_id: nil })
 
 # Test url helpers in console:
 include Rails.application.routes.url_helpers
