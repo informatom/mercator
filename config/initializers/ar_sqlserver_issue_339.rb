b37bf626ffa39b44a27f2d239c78779da95e238d
Stefan Haslinger <stefan.haslinger@mittenin.at>
2016-03-05 14:19:38 +0100

    attempt 6: hopefully fixes trigger error after upgrade to sqlserveradapter 4.2

--- config/initializers/ar_sqlserver_issue_339.rb
+++ config/initializers/ar_sqlserver_issue_339.rb
@@ -1,5 +1,5 @@
 # update to 4.2. requires that additional fix according to:
 # https://github.com/rails-sqlserver/activerecord-sqlserver-adapter/issues/381#issuecomment-76366715
 if ActiveRecord::Base.connection.class.name.match(/SQLServerAdapter/)
-  ActiveRecord::ConnectionAdapters::SQLServerAdapter.use_output_inserted = false
+  ActiveRecord::ConnectionAdapters::SQLServerAdapter.use_output_inserted = true
 end
