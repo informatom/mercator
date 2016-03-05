92e20c73425e9490a1473f93236e1370feba7fce
Stefan Haslinger <stefan.haslinger@mittenin.at>
2016-03-05 13:28:24 +0100

    attempt 2: hopefully fixes trigger error after upgrade to sqlserveradapter 4.2

--- config/initializers/ar_sqlserver_issue_339.rb
+++ config/initializers/ar_sqlserver_issue_339.rb
@@ -3,15 +3,15 @@ module ActiveRecord
     module Sqlserver
       module DatabaseStatements
 
-        def sql_for_insert(sql, pk, id_value, sequence_name, binds)
-          ["#{sql}; SELECT CAST(SCOPE_IDENTITY() AS bigint) AS Ident", binds]
-        end
+#        def sql_for_insert(sql, pk, id_value, sequence_name, binds)
+#          ["#{sql}; SELECT CAST(SCOPE_IDENTITY() AS bigint) AS Ident", binds]
+#        end
 
 # update to 4.2. requires that additional fix according to:
 # https://github.com/rails-sqlserver/activerecord-sqlserver-adapter/issues/381#issuecomment-76366715
-        if ActiveRecord::Base.connection.class.name.match(/SQLServerAdapter/)
-          ActiveRecord::ConnectionAdapters::SQLServerAdapter.use_output_inserted = false
-        end
+#        if ActiveRecord::Base.connection.class.name.match(/SQLServerAdapter/)
+#          ActiveRecord::ConnectionAdapters::SQLServerAdapter.use_output_inserted = false
+#        end
 
       end
     end
