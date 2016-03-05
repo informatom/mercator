cb33e1ee121b3c92546376da42b083d34d42fef1
Stefan Haslinger <stefan.haslinger@mittenin.at>
2016-03-05 14:03:43 +0100

    attempt 5: hopefully fixes trigger error after upgrade to sqlserveradapter 4.2

--- config/initializers/ar_sqlserver_issue_339.rb
+++ config/initializers/ar_sqlserver_issue_339.rb
@@ -1,29 +1,5 @@
-module ActiveRecord
-  module ConnectionAdapters
-    module Sqlserver
-      module DatabaseStatements
-
-       def sql_for_insert(sql, pk, id_value, sequence_name, binds)
-         sql = if pk && self.class.use_output_inserted
-         quoted_pk = SQLServer::Utils.extract_identifiers(pk).quoted
-           sql.insert sql.index(/ (DEFAULT )?VALUES/), " OUTPUT INSERTED.#{quoted_pk}"
-         else
-           "#{sql}; SELECT CAST(SCOPE_IDENTITY() AS bigint) AS Ident"
-         end
-         super
-       end
-
-#        def sql_for_insert(sql, pk, id_value, sequence_name, binds)
-#          ["#{sql}; SELECT CAST(SCOPE_IDENTITY() AS bigint) AS Ident", binds]
-#        end
-
 # update to 4.2. requires that additional fix according to:
 # https://github.com/rails-sqlserver/activerecord-sqlserver-adapter/issues/381#issuecomment-76366715
-#        if ActiveRecord::Base.connection.class.name.match(/SQLServerAdapter/)
-#          ActiveRecord::ConnectionAdapters::SQLServerAdapter.use_output_inserted = false
-#        end
-
-      end
-    end
-  end
+if ActiveRecord::Base.connection.class.name.match(/SQLServerAdapter/)
+  ActiveRecord::ConnectionAdapters::SQLServerAdapter.use_output_inserted = false
 end
